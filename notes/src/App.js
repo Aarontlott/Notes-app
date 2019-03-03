4769119b6354203c18159e7453631b22e8e62acd
Aaron Lott <aarontlott@gmail.com>
2019-03-03 21:13:05 +0000

    FEAT (Add Notes) Adding notes and passing up clicked

--- notes/src/App.js
+++ notes/src/App.js
@@ -9,26 +9,26 @@ class App extends Component {
         {
           heading: 'Test heading 1',
           text: 'Repellat nulla et facere consequatur aut consectetur.',
-          date: Moment('03/02/2019, 09:54:21').format('Do MMM YYYY'),
-          id: '1551608446446'
+          date: 1547942400000,
+          active: false
         },
         {
           heading: 'Test heading 2',
           text: 'Repellat nulla et facere consequatur aut consectetur.',
-          date: Moment('03/01/2019, 09:54:21').format('Do MMM YYYY'),
-          id: '1551608454660'
+          date: 1549152000000,
+          active: false
         },
         {
           heading: 'Test heading 3',
           text: 'Repellat nulla et facere consequatur aut consectetur.',
-          date: Moment('02/25/2019, 09:54:21').format('Do MMM YYYY'),
-          id: '1551608461557'
+          date: 1550275200000,
+          active: false
         },
         {
           heading: 'Test heading 4',
           text: 'Repellat nulla et facere consequatur aut consectetur.',
-          date: Moment('03/03/2019, 09:54:21').format('Do MMM YYYY'),
-          id: '1551608465639'
+          date: 1551312000000,
+          active: false
         }
       ], text: '' };
     
@@ -41,31 +41,45 @@ class App extends Component {
       let newNote = {
         heading: 'Here is your new note...',
         text: 'Here is your new note...',
-        date: Moment(Date.now()).format('Do MMM YYYY'),
-        id: Date.now()
+        date: Date.now(),
+        active: true
       };
       notes.push(newNote);
       this.setState(notes);
     };
 
     const notes = this.state.notes;
+    const makeActive = (e) => {
+      console.log(e.target);
+    };
 
     return (
       <div className="App">
-        <button onClick={addNote}>Add new note</button>
-        <div className="notes"><RenderNotesList notesToRender={notes} /></div>
-        <div className="note-text">Note text here</div>
+        <button onClick={addNote}>Add New Note</button>
+        <div className="notes"><RenderNotesList notesToRender={notes} onClickFunction={makeActive} /></div>
+        <div className="note-text"><NoteTextArea /></div>
       </div>
     );
   }
 }
 
+function NoteTextArea(props) {
+  return(
+    <textarea>
+      {props.text}
+    </textarea>
+  );
+}
 
 function RenderNotesList(props) {
+  props.notesToRender.sort(function (a, b) {
+    return b.date - a.date;
+  });
+  const handleClick = (e) => props.onClickFunction(e);
   return (
     <ul>
      {props.notesToRender.map(note => (
-          <li key={note.id}>{note.heading} <br /> <span>{note.date}</span></li>
+          <li key={note.date}><button id={note.date} onClick={handleClick}>{note.heading} <br /> <span>{Moment(note.date).format('Do MMM YYYY')}</span></button></li>
         ))}
     </ul>
   );
